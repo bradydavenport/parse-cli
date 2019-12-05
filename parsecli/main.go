6ab507dc59c3465c36a7ccd13f4937650b778b73
nataliaconde <natalia.csouza@outlook.com>
2019-12-05 12:30:40 -0300

    Change cloud function to 3.x

--- parsecli/main.go
+++ parsecli/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	Version        = "3.2.0"
+	Version        = "3.2.1"
 	CloudDir       = "cloud"
 	HostingDir     = "public"
 	DefaultBaseURL = "https://parsecli.back4app.com/"
