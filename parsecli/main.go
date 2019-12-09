14367fbc1a573b60bdd9a3ec1e27d96f7353e2dd
Natália Condê <32825972+nataliaconde@users.noreply.github.com>
2019-12-09 10:06:45 -0300

    Update cli version to 3.3.0

--- parsecli/main.go
+++ parsecli/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	Version        = "3.2.1"
+	Version        = "3.3.0"
 	CloudDir       = "cloud"
 	HostingDir     = "public"
 	DefaultBaseURL = "https://parsecli.back4app.com/"
